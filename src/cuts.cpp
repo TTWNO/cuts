d367aa5129aff8364e969b16028fcdc940216e9c
Tait Hoyem <taitskywalker@gmail.com>
2019-03-16 02:46:15 +0000

    Add -d/t/s, -D/T/S and colon format.

--- src/cuts.cpp
+++ src/cuts.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
 #include <string>
 #include "functions.h"
-#include <catch2/catch.hpp>
 
 using namespace std;
 
 int main(int argc, char* argv[]){
 	for (int i = 1; i< argc; i++){
 		string arg = argv[i];
-		if(arg == "-d" || arg == "--delimiter"){
+		if(arg == "-d" || arg == "-t" || arg == "-s"){
 			if (i+1 < argc){
 				string dlim = argv[++i];
-				cout << "Delimiter: '" << dlim << "'" << endl;
-		}}
+				cout << "Input delimiter: '" << dlim << "'" << endl;
+			}
+		}
+		if (arg == "-D" || arg == "-T" || arg == "-S"){
+			if (i+1 < argc){
+				string dlim = argv[++i];
+				cout << "Output delimiter: '" << dlim << "'" << endl;
+			}
+		}
+		// Last option always. For performance sake.
+		if (arg.find(":") != string::npos){
+			cout << "Colon mode!" << endl;
+			vector<string> info = delimit_string(arg, ":");
+			string colomns = info.at(1);
+			string filename = info.at(0);
+			if (file_exists(filename)){
+				cout << "File: " << filename << endl;
+				cout << "Coloumns: " << colomns << endl;
+			}
+		}
+		if (file_exists(arg)){
+			cout << "File: " << arg << endl;
+		}
 	}
 	return 0;
 }
