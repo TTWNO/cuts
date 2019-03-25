d9373b52fb99e7ee702cb53557db3597b33c434f
Tait Hoyem <44244401+TTWNO@users.noreply.github.com>
2019-03-25 06:14:04 +0000

    Start moving class code into seperate header and .cpp

---
+++ src/classes.h
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <boost/regex>
+
+class FieldsAndColumns {
+	vector<int> filter_nums;
+	vector<string> fields;
+	vector<string> filtered_fields;
+	string data;
+	string str_delimiter;
+	boost::regex re_delimiter;
+	public:
+		void set_fields(vector<string> new_fields);
+		void set_filter_nums(vector<int> new_columns);
+		void set_string_delimiter(string new_str_delimiter);
+		void set_data(string new_data);
+		void set_regex_delimiter(boost::regex re);
+
+		vector<string> get_fields();
+		string get_data();
+		boost::regex get_regex_delimiter();
+		vector<string> delimit_data_by_string();
+		vector<int> get_filter_nums();
+		string get_string_delimiter();
+
+		vector<string> delimit_data_by_regex();
+		vector<string> match_data_by_regex(boost::regex re);
+		vector<string> get_filtered_fields();
+};
+
+
+class File {
+	public:
+		string name;
+		string contents;
+		FieldAndColumns fields_and_columns;
+}
